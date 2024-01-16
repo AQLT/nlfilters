c835222444486f20a15323ad59b2f2eb67f9d81f
AQLT <alain.quartier@yahoo.fr>
2024-01-16 18:59:44 +0100

    runmed

--- test.R
+++ test.R
@@ -10,9 +10,11 @@ y_simul <- y_simul[[length(y_simul)]]
 h_f <- y_obs * hf
 
 med_f <- pracma::hampel(y_obs, 6, 0)
+med_f <- med_f$y
+med_f <- ts(runmed(y_obs, 9), start = start(y_obs), frequency = frequency(y_obs))
 hp_f <- pracma::hampel(y_obs, 6)
 
-data <- ts.union(y_obs, h_f, med_f$y, hp_f$y)
+data <- ts.union(y_obs, h_f, med_f, hp_f$y)
 colnames(data) <- c("y", "Henderson", "Median filter", "Hampel")
 
 AQLTools::graph_ts(window(data, start = 2000, end = c(2001,12)),size = 1) /
